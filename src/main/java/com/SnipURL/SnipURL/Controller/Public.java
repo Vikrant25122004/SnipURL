e2c51422c727708bca4498f0086bd9cfaa6061ae
Vikrant <vikrantsharma232529@gmail.com>
2025-05-13 16:09:37 +0530

    Some changes

--- src/main/java/com/SnipURL/SnipURL/Controller/Public.java
+++ src/main/java/com/SnipURL/SnipURL/Controller/Public.java
@@ -12,9 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Public {
@@ -25,7 +23,7 @@ public class Public {
     @Autowired
     private JWT_UTILS jwtUtils;
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody User user){
+    public ResponseEntity<?> signup(@RequestBody User user){
         try {
             userService.signupuser(user);
             return new ResponseEntity<>(HttpStatus.OK);
@@ -35,7 +33,7 @@ public class Public {
     }
 
     @PostMapping("/login")
-    public ResponseEntity Loginrep(@RequestBody  User user, HttpServletResponse response){
+    public ResponseEntity<?> Loginrep(@RequestBody  User user, HttpServletResponse response){
         try{
             UserDetails userDetails = userDetailService.loadUserByUsername(user.getUsername());
             String jwt = jwtUtils.generateToken(userDetails.getUsername());
@@ -47,6 +45,32 @@ public class Public {
             response.addCookie(cookie);
             return new ResponseEntity(HttpStatus.OK);
         } catch (UsernameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @PostMapping("/forgot/{username}")
+    public ResponseEntity forgot(@PathVariable String username){
+        try {
+            userService.forgot(username);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    @PostMapping("/enterotp/{username}")
+    public ResponseEntity enterotp(@PathVariable String username, @RequestParam String otp, @RequestParam String password){
+        try {
+            if (
+            userService.enterotp(username,otp)==true){
+                userService.updatepassword(username, password);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            else {
+                return new ResponseEntity<>("incorrect otp",HttpStatus.BAD_REQUEST);
+            }
+
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
--- src/main/java/com/SnipURL/SnipURL/Services/User_Service.java
+++ src/main/java/com/SnipURL/SnipURL/Services/User_Service.java
@@ -2,9 +2,12 @@ package com.SnipURL.SnipURL.Services;
 
 import com.SnipURL.SnipURL.Entity.User;
 import com.SnipURL.SnipURL.Repository.User_Repository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
 
 @Service
 public class User_Service  {
@@ -14,6 +17,9 @@ public class User_Service  {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private EmailServices emailServices;
+    @Autowired
+    private Redis_Service redisService;
+
 
     public void signupuser(User user){
         try {
@@ -25,5 +31,61 @@ public class User_Service  {
             throw new RuntimeException("something went wrong");
         }
 
+    }
+
+    public boolean forgot(String username) {
+        try {
+            User user = redisService.get(username,User.class);
+            if (user==null){
+                User user1 = user_repository.findByusername(username);
+                SecureRandom random = new SecureRandom();
+                StringBuilder otp = new StringBuilder();
+
+                for (int i = 0; i < 6; i++) {
+                    int digit = random.nextInt(10);  // generates a digit from 0 to 9
+                    otp.append(digit);
+                }
+                emailServices.messages(user1.getEmail(),"Forgot Password",otp.toString());
+                redisService.setLog("OTP_" + username, otp,300L);
+                return true;
+            }
+            else {
+
+                SecureRandom random = new SecureRandom();
+                StringBuilder otp = new StringBuilder();
+
+                for (int i = 0; i < 9; i++) {
+                    int digit = random.nextInt(10);  // generates a digit from 0 to 9
+                    otp.append(digit);
+                }
+                emailServices.messages(user.getEmail(),"Forgot Password",otp.toString());
+                redisService.setLog("OTP_" + username, otp.toString(),300L);
+                String oo= redisService.get("OTP_" + username,String.class);
+                System.out.println(oo);
+                return true;
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public boolean enterotp(String username,String otp){
+        try {
+            String storedotp = redisService.get("OTP_" + username , String.class);
+            if (otp == storedotp){
+
+                return true;
+            }
+            else {
+                return false;
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void updatepassword(String username,String password){
+        User user = user_repository.findByusername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user_repository.save(user);
     }
 }
